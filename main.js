4df3187f27d0fa7246353ff7e830ca1a70f1d679
Tommy07-coder <tosacr@alumnatflorida.es>
2025-11-22 19:06:14 +0100

    num2 agregado por usuario

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-
+readline = require("readline-sync");
 console.log("Hola Mundo");
 
-let numero1,numero2;
+let numero1, numero2;
 
 numero1 = 0;
+numero2 = readline.questionInt("Ingrese un numero: ");
+console.log("El numero ingresado es: " + numero2);
