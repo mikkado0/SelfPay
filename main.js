c6654dad02495e34802ca344be9418d695c6f75d
Tommy07-coder <tosacr@alumnatflorida.es>
2025-11-22 19:11:05 +0100

    operacion

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ let numero1, numero2;
 numero1 = 0;
 numero2 = readline.questionInt("Ingrese un numero: ");
 console.log("El numero ingresado es: " + numero2);
+numero1 = numero2 * 2;
+console.log("El doble del numero ingresado es: " + numero1);
+console.log("Fin del programa");
