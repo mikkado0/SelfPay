7de015669275952b61312157e5d0dcf96a5cd57c
mikado <45886825+mikkado0@users.noreply.github.com>
2026-01-30 18:08:57 +0100

    Añadido Morgan

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express"); //importar express
 const app = express();
-
+const morgan = require("morgan");
 app.set("port", process.env.PORT || 8080);//definir puerto del servidor 
 
 app.listen(app.get("port"), () => { // funcion para iniciar el servidor
   console.log(`Hola desde el puerto ${app.get("port")}`);
 });
 
+app.use(morgan("dev"));
+
 app.use(require("./src/routes/indexRutas"));
+
